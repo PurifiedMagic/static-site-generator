e90dd8c87026fb0006f1272fc5fcaa71d6553906
PurifiedMagic <ePurifiedMagic@gmail.com>
2024-09-13 22:44:12 +1000

    v0.1.4: Added ParentNodes and unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,12 +20,31 @@ class HTMLNode():
         )
     
     def __repr__(self):
-        print(f"HTML ag: {self.tag}",
+        print(f"HTML tag: {self.tag}",
               f"Tag content: {self.value}",
               f"Child nodes: {self.children}",
               f"HTML node attributes: {self.props}"
               )
-        
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, value=None, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None or self.tag == "":
+            raise ValueError("ParentNode must have a tag")
+        if self.children is None:
+            raise ValueError("ParentNode must have at least one LeafNode (child node)")
+        nodes = f"<{self.tag}{self.props_to_html()}>"
+        for child in self.children:
+            nodes += child.to_html()
+        nodes += f"</{self.tag}>"
+        return nodes
+    
+    def __repr__(self):
+        return f"{self.tag}, {self.children}, {self.props}"
+
+
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, children=None, props=None):
         super().__init__(tag, value, None, props)
@@ -33,8 +52,13 @@ class LeafNode(HTMLNode):
     def to_html(self):
         if self.value is None:
             raise ValueError("Value cannot be NoneType. All leaf nodes must have a value")
-        if self.tag is None:
+        if self.tag is None or len(self.tag) == 0:
             return str(self.value)
         if self.children is not None:
             return ValueError("LeafNode cannot have children")
-        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        if self.props is None or len(self.props) == 0:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.children}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, ParentNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
 
@@ -57,6 +57,81 @@ class TestHTMLNode(unittest.TestCase):
         )
         print(node.props_to_html())
         self.assertIsNot(node.props_to_html(), "")
+
+class TestParentNode(unittest.TestCase):
+    def test_parent_tag(self):
+        node = ParentNode(
+            None,
+            None,
+            [
+                "li",
+                "li",
+                "li"
+            ],
+            {
+                "class": "ordered_list_style"
+            }
+        )
+        self.assertRaises(ValueError)
+
+    def test_no_children(self):
+        node = ParentNode(
+            "ul",
+            None,
+            [],
+            None
+        )
+        node2 = ParentNode(
+            "ol",
+            None,
+            None,
+            None
+        )
+        self.assertRaises(ValueError)
+
+    def test_parent_children(self):
+        node = ParentNode(
+            "ul",
+            [
+                LeafNode(
+                    "li",
+                    "list item one",
+                    None, 
+                    {
+                        "class": "list-item"
+                    }
+                ),
+                LeafNode(
+                    "li",
+                    "list item two",
+                    [
+                        LeafNode(
+                            "strong",
+                            None,
+                            None,
+                            None
+                        )
+                    ]
+                ),
+                LeafNode(
+                    "li",
+                    "list item three",
+                    [
+                        LeafNode(
+                            "a",
+                            "Boot.dev",
+                            None,
+                            {
+                                "href": "https://www.boot.dev",
+                                "aria-label": 
+                                "Boot dot dev, gamifying back-end development concepts"
+                            }
+                        )
+                    ]
+                )
+            ]
+        )
+        print(f"{node.to_html()}")
 
 class TestLeafNode(unittest.TestCase):
 
